9ca74f8e7ade00a7f4b0cbeed500628a4b766e53
Abhinav Sharma <abhi18av@gmail.com>
2017-10-11 04:29:28 +0530

    added the resources to be used in HomeShick

--- trial1.ps1
+++ trial1.ps1
@@ -1,5 +1,20 @@
-Plaster Pester psake
+#  Plaster Pester psake
+
+# https://kevinmarquette.github.io/2017-05-27-Powershell-module-building-basics/
 
 
+# http://ramblingcookiemonster.github.io/Building-A-PowerShell-Module/
+
+# http://www.tomsitpro.com/articles/powershell-modules,2-846.html
 
 Get-Module -All
+
+
+
+Get-Command -Module Plaster
+
+
+Get-PlasterTemplate
+
+
+New-ModuleManifest 
